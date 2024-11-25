995dde15e04c7aaa442eaaa6e99c2abfb8f6ac09
kath0809 <kath014@student.kristiania.no>
2024-11-25 16:58:29 +0100

    Initial Commit

---
+++ PG5602_H24-4/PG5602_H24_4App.swift
@@ -0,0 +1,17 @@
+//
+//  PG5602_H24_4App.swift
+//  PG5602_H24-4
+//
+//  Created by Karima Thingvold on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PG5602_H24_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
