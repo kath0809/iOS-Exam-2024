0b6464ebad3c31b19cfb388ef8da975efa5b2722
kath0809 <kath014@student.kristiania.no>
2024-12-09 12:29:03 +0100

    Added news logos to splashScreen

--- PG5602_H24-4/Screens/SplashScreenView.swift
+++ PG5602_H24-4/Screens/SplashScreenView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct SplashScreen: View {
-    @State private var isActive = false
-    @State private var rotation: Double = 0
-    @State private var text1: CGFloat = -300
-    @State private var text2: CGFloat = 500
-
+    @State var isActive = false
+    @State var rotation: Double = 0
+    @State var text1: CGFloat = -300
+    @State var text2: CGFloat = 500
+    @State var showNewsImages = false
+    
     var body: some View {
         if isActive {
             MainLayoutView()
@@ -22,7 +23,7 @@ struct SplashScreen: View {
                     .edgesIgnoringSafeArea(.all)
                 
                 VStack {
-                    Text("Globe Wire")
+                    Text("GlobeWire")
                         .font(.largeTitle)
                         .foregroundStyle(.splashScreen)
                         .offset(y: text1)
@@ -32,7 +33,7 @@ struct SplashScreen: View {
                             }
                         }
                         .padding(.bottom, 20)
-                
+                    
                     Image("SplashImage")
                         .resizable()
                         .scaledToFit()
@@ -42,12 +43,12 @@ struct SplashScreen: View {
                             withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
                                 rotation = 360
                             }
-
+                            
                             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                                 isActive = true
                             }
                         }
-            
+                    
                     Text("First with all the latest news")
                         .font(.title)
                         .foregroundStyle(.splashScreen)
@@ -58,6 +59,35 @@ struct SplashScreen: View {
                             }
                         }
                         .padding(.top, 20)
+                    
+                    HStack(alignment: .center, spacing: 30) {
+                        Image("News1")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .scaleEffect(showNewsImages ? 1 : 0.5)
+                            .opacity(showNewsImages ? 1 : 0)
+                            .animation(.easeOut(duration: 1).delay(0.6), value: showNewsImages)
+                        
+                        Image("News2")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .scaleEffect(showNewsImages ? 1 : 0.5)
+                            .opacity(showNewsImages ? 1 : 0)
+                            .animation(.easeOut(duration: 1).delay(0.7), value: showNewsImages)
+                        
+                        Image("News3")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .scaleEffect(showNewsImages ? 1 : 0.5)
+                            .opacity(showNewsImages ? 1 : 0)
+                            .animation(.easeOut(duration: 1).delay(0.8), value: showNewsImages)
+                    }
+                    .padding(.top, 100)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            showNewsImages = true
+                        }
+                    }
                 }
             }
         }
